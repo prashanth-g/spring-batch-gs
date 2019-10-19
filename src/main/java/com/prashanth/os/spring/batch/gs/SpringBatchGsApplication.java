f847ae6767dafc81fbafebc3b0e56d94cc90a96b
prashanth-g <prashanthcsgce@gmail.com>
2019-10-19 20:58:24 +0530

    Initial commit

---
+++ src/main/java/com/prashanth/os/spring/batch/gs/SpringBatchGsApplication.java
@@ -0,0 +1,13 @@
+package com.prashanth.os.spring.batch.gs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchGsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchGsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prashanth/os/spring/batch/gs/SpringBatchGsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashanth.os.spring.batch.gs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchGsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
